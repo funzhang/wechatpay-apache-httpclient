ad0471d6934f9cf12f8215146cdc513409dc9ae1
Fun <234181517@qq.com>
2022-04-18 22:05:22 +0800

    For android adaption.

--- src/main/java/com/wechat/pay/contrib/apache/httpclient/SignatureExec.java
+++ src/main/java/com/wechat/pay/contrib/apache/httpclient/SignatureExec.java
@@ -4,7 +4,6 @@ import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.apache.http.HttpStatus.SC_MULTIPLE_CHOICES;
 import static org.apache.http.HttpStatus.SC_OK;
 
-import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
@@ -17,69 +16,79 @@ import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.execchain.ClientExecChain;
 
+import java.io.IOException;
+
 /**
  * @author xy-peng
  */
 public class SignatureExec implements ClientExecChain {
 
-    private static final String WECHAT_PAY_HOST_NAME_SUFFIX = ".mch.weixin.qq.com";
-    private final ClientExecChain mainExec;
-    private final Credentials credentials;
-    private final Validator validator;
+  private static final String WECHAT_PAY_HOST_NAME_SUFFIX = ".mch.weixin.qq.com";
+  private final ClientExecChain mainExec;
+  private final Credentials credentials;
+  private final Validator validator;
 
-    protected SignatureExec(Credentials credentials, Validator validator, ClientExecChain mainExec) {
-        this.credentials = credentials;
-        this.validator = validator;
-        this.mainExec = mainExec;
+  protected SignatureExec(Credentials credentials, Validator validator, ClientExecChain mainExec) {
+    this.credentials = credentials;
+    this.validator = validator;
+    this.mainExec = mainExec;
+  }
+
+  protected void convertToRepeatableResponseEntity(CloseableHttpResponse response)
+      throws IOException {
+    HttpEntity entity = response.getEntity();
+    if (entity != null) {
+      response.setEntity(new BufferedHttpEntity(entity));
     }
+  }
 
-    protected void convertToRepeatableResponseEntity(CloseableHttpResponse response) throws IOException {
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            response.setEntity(new BufferedHttpEntity(entity));
-        }
+  protected void convertToRepeatableRequestEntity(HttpRequestWrapper request) throws IOException {
+    if (request instanceof HttpEntityEnclosingRequest) {
+      HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
+      if (entity != null) {
+        ((HttpEntityEnclosingRequest) request).setEntity(new BufferedHttpEntity(entity));
+      }
     }
+  }
 
-    protected void convertToRepeatableRequestEntity(HttpRequestWrapper request) throws IOException {
-        if (request instanceof HttpEntityEnclosingRequest) {
-            HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
-            if (entity != null) {
-                ((HttpEntityEnclosingRequest) request).setEntity(new BufferedHttpEntity(entity));
-            }
-        }
+  @Override
+  public CloseableHttpResponse execute(
+      HttpRoute route,
+      HttpRequestWrapper request,
+      HttpClientContext context,
+      HttpExecutionAware execAware)
+      throws IOException, HttpException {
+    if (request.getURI().getHost().endsWith(WECHAT_PAY_HOST_NAME_SUFFIX)) { // TODO funz
+      return executeWithSignature(route, request, context, execAware);
+    } else {
+      return mainExec.execute(route, request, context, execAware);
     }
+  }
 
-    @Override
-    public CloseableHttpResponse execute(HttpRoute route, HttpRequestWrapper request, HttpClientContext context,
-            HttpExecutionAware execAware) throws IOException, HttpException {
-        if (request.getTarget().getHostName().endsWith(WECHAT_PAY_HOST_NAME_SUFFIX)) {
-            return executeWithSignature(route, request, context, execAware);
-        } else {
-            return mainExec.execute(route, request, context, execAware);
-        }
+  private CloseableHttpResponse executeWithSignature(
+      HttpRoute route,
+      HttpRequestWrapper request,
+      HttpClientContext context,
+      HttpExecutionAware execAware)
+      throws IOException, HttpException {
+    // 上传类不需要消耗两次故不做转换
+    if (!(request.getOriginal() instanceof WechatPayUploadHttpPost)) {
+      convertToRepeatableRequestEntity(request);
     }
+    // 添加认证信息
+    request.addHeader(AUTHORIZATION, credentials.getSchema() + " " + credentials.getToken(request));
 
-    private CloseableHttpResponse executeWithSignature(HttpRoute route, HttpRequestWrapper request,
-            HttpClientContext context,
-            HttpExecutionAware execAware) throws IOException, HttpException {
-        // 上传类不需要消耗两次故不做转换
-        if (!(request.getOriginal() instanceof WechatPayUploadHttpPost)) {
-            convertToRepeatableRequestEntity(request);
-        }
-        // 添加认证信息
-        request.addHeader(AUTHORIZATION, credentials.getSchema() + " " + credentials.getToken(request));
+    // 执行
+    CloseableHttpResponse response = mainExec.execute(route, request, context, execAware);
 
-        // 执行
-        CloseableHttpResponse response = mainExec.execute(route, request, context, execAware);
-
-        // 对成功应答验签
-        StatusLine statusLine = response.getStatusLine();
-        if (statusLine.getStatusCode() >= SC_OK && statusLine.getStatusCode() < SC_MULTIPLE_CHOICES) {
-            convertToRepeatableResponseEntity(response);
-            if (!validator.validate(response)) {
-                throw new HttpException("应答的微信支付签名验证失败");
-            }
-        }
-        return response;
+    // 对成功应答验签
+    StatusLine statusLine = response.getStatusLine();
+    if (statusLine.getStatusCode() >= SC_OK && statusLine.getStatusCode() < SC_MULTIPLE_CHOICES) {
+      convertToRepeatableResponseEntity(response);
+      if (!validator.validate(response)) {
+        throw new HttpException("应答的微信支付签名验证失败");
+      }
     }
+    return response;
+  }
 }
