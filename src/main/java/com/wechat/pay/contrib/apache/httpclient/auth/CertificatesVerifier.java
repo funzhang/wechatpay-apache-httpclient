a2cda4bce4fefe16ee58ba66237b3d3581d4aaa8
Fun <234181517@qq.com>
2022-06-14 22:52:17 +0800

    Adapt api 25.

--- src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/CertificatesVerifier.java
+++ src/main/java/com/wechat/pay/contrib/apache/httpclient/auth/CertificatesVerifier.java
@@ -1,5 +1,7 @@
 package com.wechat.pay.contrib.apache.httpclient.auth;
 
+import android.util.Base64;
+
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -8,7 +10,6 @@ import java.security.SignatureException;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,62 +20,60 @@ import java.util.NoSuchElementException;
  */
 public class CertificatesVerifier implements Verifier {
 
-    protected final HashMap<BigInteger, X509Certificate> certificates = new HashMap<>();
+  protected final HashMap<BigInteger, X509Certificate> certificates = new HashMap<>();
 
-    public CertificatesVerifier(List<X509Certificate> list) {
-        for (X509Certificate item : list) {
-            certificates.put(item.getSerialNumber(), item);
-        }
+  public CertificatesVerifier(List<X509Certificate> list) {
+    for (X509Certificate item : list) {
+      certificates.put(item.getSerialNumber(), item);
     }
+  }
 
-    public CertificatesVerifier(Map<BigInteger, X509Certificate> certificates) {
-        this.certificates.putAll(certificates);
+  public CertificatesVerifier(Map<BigInteger, X509Certificate> certificates) {
+    this.certificates.putAll(certificates);
+  }
+
+  public void updateCertificates(Map<BigInteger, X509Certificate> certificates) {
+    this.certificates.clear();
+    this.certificates.putAll(certificates);
+  }
+
+  protected boolean verify(X509Certificate certificate, byte[] message, String signature) {
+    try {
+      Signature sign = Signature.getInstance("SHA256withRSA");
+      sign.initVerify(certificate);
+      sign.update(message);
+      return sign.verify(Base64.decode(signature, Base64.DEFAULT));
+
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("当前Java环境不支持SHA256withRSA", e);
+    } catch (SignatureException e) {
+      throw new RuntimeException("签名验证过程发生了错误", e);
+    } catch (InvalidKeyException e) {
+      throw new RuntimeException("无效的证书", e);
     }
+  }
 
+  @Override
+  public boolean verify(String serialNumber, byte[] message, String signature) {
+    BigInteger val = new BigInteger(serialNumber, 16);
+    X509Certificate cert = certificates.get(val);
+    return cert != null && verify(cert, message, signature);
+  }
 
-    public void updateCertificates(Map<BigInteger, X509Certificate> certificates) {
-        this.certificates.clear();
-        this.certificates.putAll(certificates);
+  @Override
+  public X509Certificate getValidCertificate() {
+    X509Certificate latestCert = null;
+    for (X509Certificate x509Cert : certificates.values()) {
+      // 若latestCert为空或x509Cert的证书有效开始时间在latestCert之后，则更新latestCert
+      if (latestCert == null || x509Cert.getNotBefore().after(latestCert.getNotBefore())) {
+        latestCert = x509Cert;
+      }
     }
-
-    protected boolean verify(X509Certificate certificate, byte[] message, String signature) {
-        try {
-            Signature sign = Signature.getInstance("SHA256withRSA");
-            sign.initVerify(certificate);
-            sign.update(message);
-            return sign.verify(Base64.getDecoder().decode(signature));
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("当前Java环境不支持SHA256withRSA", e);
-        } catch (SignatureException e) {
-            throw new RuntimeException("签名验证过程发生了错误", e);
-        } catch (InvalidKeyException e) {
-            throw new RuntimeException("无效的证书", e);
-        }
+    try {
+      latestCert.checkValidity();
+      return latestCert;
+    } catch (CertificateExpiredException | CertificateNotYetValidException e) {
+      throw new NoSuchElementException("没有有效的微信支付平台证书");
     }
-
-    @Override
-    public boolean verify(String serialNumber, byte[] message, String signature) {
-        BigInteger val = new BigInteger(serialNumber, 16);
-        X509Certificate cert = certificates.get(val);
-        return cert != null && verify(cert, message, signature);
-    }
-
-    @Override
-    public X509Certificate getValidCertificate() {
-        X509Certificate latestCert = null;
-        for (X509Certificate x509Cert : certificates.values()) {
-            // 若latestCert为空或x509Cert的证书有效开始时间在latestCert之后，则更新latestCert
-            if (latestCert == null || x509Cert.getNotBefore().after(latestCert.getNotBefore())) {
-                latestCert = x509Cert;
-            }
-        }
-        try {
-            latestCert.checkValidity();
-            return latestCert;
-        } catch (CertificateExpiredException | CertificateNotYetValidException e) {
-            throw new NoSuchElementException("没有有效的微信支付平台证书");
-        }
-    }
+  }
 }
-
--- src/main/java/com/wechat/pay/contrib/apache/httpclient/util/RsaCryptoUtil.java
+++ src/main/java/com/wechat/pay/contrib/apache/httpclient/util/RsaCryptoUtil.java
@@ -1,11 +1,13 @@
 package com.wechat.pay.contrib.apache.httpclient.util;
 
+import android.util.Base64;
+
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.Base64;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -16,38 +18,40 @@ import javax.crypto.NoSuchPaddingException;
  */
 public class RsaCryptoUtil {
 
-    private static final String TRANSFORMATION = "RSA/ECB/OAEPWithSHA-1AndMGF1Padding";
+  private static final String TRANSFORMATION = "RSA/ECB/OAEPWithSHA-1AndMGF1Padding";
 
-    public static String encryptOAEP(String message, X509Certificate certificate) throws IllegalBlockSizeException {
-        try {
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, certificate.getPublicKey());
-            byte[] data = message.getBytes(StandardCharsets.UTF_8);
-            byte[] ciphertext = cipher.doFinal(data);
-            return Base64.getEncoder().encodeToString(ciphertext);
+  public static String encryptOAEP(String message, X509Certificate certificate)
+      throws IllegalBlockSizeException {
+    try {
+      Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+      cipher.init(Cipher.ENCRYPT_MODE, certificate.getPublicKey());
+      byte[] data = message.getBytes(StandardCharsets.UTF_8);
+      byte[] ciphertext = cipher.doFinal(data);
+      return Base64.encodeToString(ciphertext, Base64.DEFAULT);
 
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
-        } catch (InvalidKeyException e) {
-            throw new IllegalArgumentException("无效的证书", e);
-        } catch (IllegalBlockSizeException | BadPaddingException e) {
-            throw new IllegalBlockSizeException("加密原串的长度不能超过214字节");
-        }
+    } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+      throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
+    } catch (InvalidKeyException e) {
+      throw new IllegalArgumentException("无效的证书", e);
+    } catch (IllegalBlockSizeException | BadPaddingException e) {
+      throw new IllegalBlockSizeException("加密原串的长度不能超过214字节");
     }
+  }
 
-    public static String decryptOAEP(String ciphertext, PrivateKey privateKey) throws BadPaddingException {
-        try {
-            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            byte[] data = Base64.getDecoder().decode(ciphertext);
-            return new String(cipher.doFinal(data), StandardCharsets.UTF_8);
+  public static String decryptOAEP(String ciphertext, PrivateKey privateKey)
+      throws BadPaddingException {
+    try {
+      Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+      cipher.init(Cipher.DECRYPT_MODE, privateKey);
+      byte[] data = Base64.decode(ciphertext, Base64.DEFAULT);
+      return new String(cipher.doFinal(data), StandardCharsets.UTF_8);
 
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
-            throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
-        } catch (InvalidKeyException e) {
-            throw new IllegalArgumentException("无效的私钥", e);
-        } catch (BadPaddingException | IllegalBlockSizeException e) {
-            throw new BadPaddingException("解密失败");
-        }
+    } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+      throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
+    } catch (InvalidKeyException e) {
+      throw new IllegalArgumentException("无效的私钥", e);
+    } catch (BadPaddingException | IllegalBlockSizeException e) {
+      throw new BadPaddingException("解密失败");
     }
+  }
 }
